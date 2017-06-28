5e8d0ca1eefe3ab8ec6be51f2847febf19fe7e95
anucha8p <anucha8p@gmail.com>
2017-06-28 13:40:48 +0700

    keep db connection alive

--- index.js
+++ index.js
@@ -14,7 +14,41 @@ var tns = require('./tns-core');
 //     database: 'tns_adsurvey_2017'
 // });
 
-var connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+//var connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+
+//var db_config = process.env.CLEARDB_DATABASE_URL;
+
+var db_config = {
+    host: 'localhost',
+    user: 'root',
+    password: 'morningM00n',
+    database: 'tns_adsurvey_2017'
+};
+
+// for keep db connection alive
+var connection;
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+handleDisconnect();
+setInterval(function () { connection.query('SELECT 1'); }, 5000);
+
 
 var S3_BUCKET = process.env.S3_BUCKET;
 
